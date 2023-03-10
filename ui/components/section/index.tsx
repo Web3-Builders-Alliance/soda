f3e1bcef8e7097f40fe90e939afd5197b0097a44
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2023-03-10 14:24:10 -0300

    ui improvements (#25)

--- ui/components/section/index.tsx
+++ ui/components/section/index.tsx
@@ -1,10 +1,15 @@
 import { FC, useState } from "react";
 import { NewItem } from "../NewItem";
 
-export const Section: FC<any> = ({ content, setContent }) => {
+export const Section: FC<any> = ({ name, content, setContent }) => {
   const [expanded, setExpanded] = useState(true);
   return (
     <section className={`flex p-5 m-5 border border-gray-900 bg-gray-700 relative`}>
+      <div
+        className="absolute flex left-5 top-0 transform  -translate-y-1/2 text-white justify-center items-center font-mono font-thin"
+      >
+        {name}
+      </div>
       <div className={`transition-all duration-500 ease-in-out overflow-x-auto overflow-y-hidden ${expanded ? "h-64" : " h-4"}`}>
       {content?.map((item: any) => (
         <div key={item.name}>{item.name}</div>
