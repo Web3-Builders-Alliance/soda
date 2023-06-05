e939feb3289c5019f0327333430cbb2269933f5d
Marche <juanpatriciomarchetto@gmail.com>
2023-06-05 20:05:00 -0300

    Saving IDL file (#57)

--- ui/src-tauri/src/main.rs
+++ ui/src-tauri/src/main.rs
@@ -3,7 +3,7 @@
     windows_subsystem = "windows"
 )]
 #![allow(non_snake_case, non_camel_case_types)]
-
+use std::io::Write;
 use soda::{generate_from_idl, IDL};
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
@@ -21,7 +21,7 @@ fn main() {
             .add_item(open_idl)
             .add_item(new_project)
             //.add_item(generate_project)
-            //.add_item(generate_idl)
+            .add_item(generate_idl)
             .add_item(change_template)
             .add_item(quit)
     );
@@ -51,12 +51,12 @@ fn main() {
                     .emit("generate_project", Some("generate_project".to_string()))
                     .unwrap();
             }*/
-            /*"generate_idl" => {
+            "generate_idl" => {
                 event
                     .window()
                     .emit("generate_idl", Some("generate_idl".to_string()))
                     .unwrap();
-            }*/
+            }
             "change_template" => {
                 event
                     .window()
@@ -67,14 +67,21 @@ fn main() {
                 std::process::exit(0);
             }
         })
-        .invoke_handler(tauri::generate_handler![generate])
+        .invoke_handler(tauri::generate_handler![generate, generate_idl_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn generate(handle: tauri::AppHandle, baseFolder: &str, idl: &str, templateFolder: &str) -> String {
+fn generate(handle: tauri::AppHandle, baseFolder: &str, idl: &str, templateFolder: &str) -> () {
     let idl: IDL = serde_json::from_str(idl).expect("error while reading json");
     generate_from_idl(baseFolder, idl, templateFolder);
-    format!("Project Generated!")
+}
+
+#[tauri::command]
+fn generate_idl_file(handle: tauri::AppHandle, baseFolder: &str, idl: &str) -> (){
+    let idl: IDL = serde_json::from_str(idl).expect("error while reading json");
+    // generate json file
+    let mut file = std::fs::File::create(format!("{}/idl.json", baseFolder)).unwrap();
+    file.write_all(serde_json::to_string_pretty(&idl).unwrap().as_bytes()).unwrap();
 }
