2f15eb502979fd7c51125c291a755e935da0c80c
SergioAriel <42455008+SergioAriel@users.noreply.github.com>
2023-06-27 12:02:09 -0300

    UI (#94)

---
+++ ui/components/JSONEditor/index.tsx
@@ -0,0 +1,35 @@
+import { useIDL } from '@/context/IDL';
+import CodeMirror from '@uiw/react-codemirror';
+import { useState } from 'react';
+import {  githubDark } from '@uiw/codemirror-theme-github';
+
+
+const JSONEditor = () => {
+    const { IDL, setIDL } = useIDL()
+    const [errorIDL, setErrorIDL] = useState("")
+    const handlerIDL = (value: any)=> {
+        try {
+            setErrorIDL("")
+            const parse = JSON.parse(value)
+            setIDL(parse)
+        } catch (error: any) {
+            setErrorIDL(error.toString())
+        }
+
+    }
+
+    console.log(errorIDL)
+
+    return (
+        <div>
+            <p className='text-red-600'>{errorIDL}</p>
+            <CodeMirror
+                value={JSON.stringify(IDL, null, "\t")}
+                onChange={handlerIDL}
+                theme={githubDark}
+            />
+        </div>
+    )
+}
+
+export default JSONEditor
