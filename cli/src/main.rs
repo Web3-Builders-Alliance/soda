03489a4e400fd00ee5b93d496dd3f77040618d3f
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-06-28 13:43:37 -0300

    upgrade crate version

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,6 +6,7 @@ use std::fs::{canonicalize, File};
 
 const IDL_DEFAULT_PATH: &str = "./idl.json";
 const TEMPLATE_DEFAULT_PATH: &str = "./template";
+const TEMPLATE_FILE_NAME: &str = "template.soda";
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -23,17 +24,26 @@ fn main() -> Result<(), Box<dyn Error>> {
                 } else {
                     TEMPLATE_DEFAULT_PATH
                 };
+                let file_path = if cli.paths.len() > 2 {
+                    &cli.paths[2]
+                } else {
+                    TEMPLATE_FILE_NAME                };
                 let template = get_template_from_fs(template_path);
-                save_template(template, "template.soda");
+                save_template(template, file_path );
             }
             command if command == "unpack-template" => {
                 let template_path = if cli.paths.len() > 1 {
                     &cli.paths[1]
                 } else {
+                    TEMPLATE_FILE_NAME
+                };
+                let base_path = if cli.paths.len() > 2 {
+                    &cli.paths[2]
+                } else {
                     TEMPLATE_DEFAULT_PATH
                 };
                 let template = load_template(template_path);
-                write_project_to_fs(template.files, ".");
+                write_project_to_fs(template.files, base_path);
                 let mut helpers = vec![];
                 for helper in template.helpers {
                     helpers.push(TemplateFile {
@@ -41,7 +51,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                         content: Content::String(helper.script),
                     });
                 }
-                write_project_to_fs(helpers, ".");
+                write_project_to_fs(helpers, base_path);
                 println!("Template Generated!");
             }
             command if command == "create-project" => {
@@ -73,7 +83,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 fn display_help() {
     println!("Commands:");
-    println!("create-project <idl_path> <template_path>");
-    println!("template-pack <template_path>");
-    println!("template-unpack <template_path>");
+    println!("  create-project [idl_path] [template_path]");
+    println!("  pack-template [template_path] [file_path]");
+    println!("  unpack-template [template_path]");
 }
