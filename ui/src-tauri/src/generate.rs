2c2e0d04acfe5317a479399602bd3be7b0f488f1
Marche <juanpatriciomarchetto@gmail.com>
2023-07-04 14:58:01 -0300

    Handle Errors (#126)

--- ui/src-tauri/src/generate.rs
+++ ui/src-tauri/src/generate.rs
@@ -3,20 +3,15 @@ use crate::*;
 #[tauri::command]
 pub fn generate(state: State<AppState>) -> Result<(), Error> {
     let (idl_string, base_folder, template) = {
-        let state = state.0.lock().unwrap();
+        let state = state.0.lock()?;
         (
             &state.idl_string.clone(),
             &state.base_folder.clone(),
             &state.template.clone(),
         )
     };
-    match serde_json::from_str::<IDL>(idl_string) {
-        Ok(idl) => {
-            let dinamyc_files = generate_project(template.clone(), &idl)?;
-            write_project_to_fs(dinamyc_files, base_folder).map_err(|e| e.into())
-        }
-        Err(e) => Err(Error::CustomError {
-            message: e.to_string(),
-        }),
-    }
+    let idl = serde_json::from_str::<IDL>(idl_string)?;
+    let dinamyc_files = generate_project(template.clone(), &idl)?;
+    write_project_to_fs(dinamyc_files, base_folder)?;
+    Ok(())
 }
